27a85c71f01348d5b55f47a0b8b9559a63f692f9
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-10-25 09:40:17 +0300

    Text parser import done.

--- src/main/java/com/mika/WineParser/Main.java
+++ src/main/java/com/mika/WineParser/Main.java
@@ -2,20 +2,21 @@ package com.mika.WineParser;
 
 import com.mika.WineApp.models.Review;
 import com.mika.WineApp.models.Wine;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 
 /** Driver class for TextParser. Gets Wines and Reviews as lists. */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         TextParser parser = new TextParser();
-        String textsDirectory = ResourceUtils.getFile("classpath:texts").getPath();
+
+        // Path to directory where the text files are:
+        String textsDirectory = ResourceUtils
+                .getFile("classpath:texts")
+                .getPath();
 
         // Parse all wines and reviews:
         try {
@@ -27,7 +28,5 @@ public class Main {
         // Parsed wines and reviews:
         List<Wine> wines = parser.getWines();
         List<Review> reviews = parser.getReviews();
-
-        wines.forEach(w -> System.out.println(w.getName()));
     }
 }
