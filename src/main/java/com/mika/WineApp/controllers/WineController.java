7dd83402e768222978891471c1246818ff440d9d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-10-25 16:34:16 +0300

    Worked on wine controller and service. TODO: deal with Optionals.

--- src/main/java/com/mika/WineApp/controllers/WineController.java
+++ src/main/java/com/mika/WineApp/controllers/WineController.java
@@ -1,4 +1,45 @@
 package com.mika.WineApp.controllers;
 
+import com.mika.WineApp.models.Wine;
+import com.mika.WineApp.repositories.WineRepository;
+import com.mika.WineApp.services.WineService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class WineController {
+    private final WineService service;
+
+    public WineController(WineRepository repository) {
+        this.service = new WineService(repository);
+    }
+
+    @GetMapping("/wines")
+    List<Wine> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("wines/{id}")
+    Wine find(@PathVariable Long id) {
+        Optional<Wine> opt = service.find(id);
+        // TODO: Handle optionals!
+        return opt.get();
+    }
+
+    @PostMapping("/wines")
+    Wine add(@RequestBody Wine newWine) {
+        return service.add(newWine);
+    }
+
+    @PutMapping("wines/{id}")
+    Wine edit(@RequestBody Wine editedWine, @PathVariable Long id) {
+        return service.edit(editedWine, id);
+    }
+
+    @DeleteMapping("wines/{id}")
+    void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
--- src/main/java/com/mika/WineApp/services/WineService.java
+++ src/main/java/com/mika/WineApp/services/WineService.java
@@ -1,4 +1,37 @@
 package com.mika.WineApp.services;
 
+import com.mika.WineApp.models.Wine;
+import com.mika.WineApp.repositories.WineRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class WineService {
+    private final WineRepository repository;
+
+    public WineService(WineRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Wine> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Wine> find(long id) {
+        return repository.findById(id);
+    }
+
+    public Wine add(Wine newWine) {
+        return repository.save(newWine);
+    }
+
+    public Wine edit(Wine wine, Long id) {
+        // TODO: Optionals
+        Optional<Wine> oldWine = repository.findById(id);
+        return wine;
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
