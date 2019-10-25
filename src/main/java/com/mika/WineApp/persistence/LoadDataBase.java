fdc0c26e3a00e278ede4ddf51532f931e13861a0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-10-25 15:56:10 +0300

    Implemented functionality for loading parsed data into a database.

--- src/main/java/com/mika/WineApp/persistence/LoadDataBase.java
+++ src/main/java/com/mika/WineApp/persistence/LoadDataBase.java
@@ -1,4 +1,32 @@
 package com.mika.WineApp.persistence;
 
-public class LoadDataBase {
+import com.mika.WineApp.repositories.ReviewRepository;
+import com.mika.WineApp.repositories.WineRepository;
+import com.mika.WineParser.Parser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@Slf4j
+class LoadDataBase {
+
+    @Bean
+    CommandLineRunner initDatabase(WineRepository wineRepository, ReviewRepository reviewRepository) {
+        // Try to parse wines and reviews into database:
+        Parser parser = new Parser();
+
+        return args -> {
+            log.info("Preloading database.");
+
+            wineRepository.saveAll(parser.getWines());
+            log.info("Wines loaded successfully!");
+
+            reviewRepository.saveAll(parser.getReviews());
+            log.info("Reviews loaded successfully!");
+        };
+    }
 }
---
+++ src/main/java/com/mika/WineParser/Parser.java
@@ -0,0 +1,47 @@
+package com.mika.WineParser;
+
+import com.mika.WineApp.models.Review;
+import com.mika.WineApp.models.Wine;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+/** Driver class for TextParser. Gets Wines and Reviews as lists. */
+public class Parser {
+    private List<Wine> wines;
+    private List<Review> reviews;
+
+    public Parser() {
+        try {
+            parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parse() throws IOException {
+        TextParser textParser = new TextParser();
+
+        // Path to directory where the text files are:
+        String textsDirectory = ResourceUtils
+                .getFile("classpath:texts")
+                .getPath();
+
+        // Parse all wines and reviews:
+        textParser.parseAll(textsDirectory);
+
+        // Parsed wines and reviews:
+        this.wines = textParser.getWines();
+        this.reviews = textParser.getReviews();
+    }
+
+    public List<Wine> getWines() {
+        return wines;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+}
