dc74a0b66aaa2e014f8310f7be1922bc462d08cc
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-10-25 07:49:48 +0300

    Created Spring Boot project.

---
+++ src/main/java/com/mika/WineApp/WineAppApplication.java
@@ -0,0 +1,13 @@
+package com.mika.WineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mika/WineApp/WineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mika.WineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
