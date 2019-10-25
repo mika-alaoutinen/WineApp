c08d35b39ffd7d33e57fb85464f5254a24d5aef2
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2019-10-25 15:33:14 +0300

    Decided that adding in a persisted Author model is probably a bad idea. (#3)

--- src/main/java/com/mika/WineApp/models/Author.java
+++ src/main/java/com/mika/WineApp/models/Author.java
@@ -6,8 +6,18 @@ import javax.persistence.Entity;
 import java.util.List;
 import java.util.Set;
 
+/*
+TODO:
+  is this model really needed?
+
+  You can find a list of reviews by a specific
+  author from querying ReviewRepository. At least Author shouldn't be persisted
+  in a database, because the information is redundant!
+
+  Maybe generate an author in memory when reviews are queried by author?
+*/
+
 @Data
-@Entity
 public class Author {
     private String name;
     private List<Review> reviews;
