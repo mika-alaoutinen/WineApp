adb1a24f19575841ae881d79baf67939dede2c0e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-10-25 09:33:52 +0300

    Parsing texts works. Consider refactoring.

--- src/main/java/com/mika/WineParser/Main.java
+++ src/main/java/com/mika/WineParser/Main.java
@@ -2,17 +2,20 @@ package com.mika.WineParser;
 
 import com.mika.WineApp.models.Review;
 import com.mika.WineApp.models.Wine;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 /** Driver class for TextParser. Gets Wines and Reviews as lists. */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         TextParser parser = new TextParser();
-
-        // Important: set the path to the directory where the text files are here!
-        String textsDirectory = "texts";
+        String textsDirectory = ResourceUtils.getFile("classpath:texts").getPath();
 
         // Parse all wines and reviews:
         try {
@@ -24,5 +27,7 @@ public class Main {
         // Parsed wines and reviews:
         List<Wine> wines = parser.getWines();
         List<Review> reviews = parser.getReviews();
+
+        wines.forEach(w -> System.out.println(w.getName()));
     }
 }
