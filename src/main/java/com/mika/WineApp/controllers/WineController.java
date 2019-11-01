5e01ec58dba34a6407aba32b10079c73e65d7354
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-01 19:02:43 +0200

    Changed api endpoints to be unique and started implementing hateoas.

--- src/main/java/com/mika/WineApp/controllers/WineController.java
+++ src/main/java/com/mika/WineApp/controllers/WineController.java
@@ -5,6 +5,8 @@ import com.mika.WineApp.models.Wine;
 import com.mika.WineApp.models.WineType;
 import com.mika.WineApp.repositories.WineRepository;
 import com.mika.WineApp.services.WineService;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,41 +21,52 @@ public class WineController {
     }
 
     @GetMapping("/wines")
-    List<Wine> findAll(@RequestParam(name = "minPrice") OptionalDouble minPrice,
-                       @RequestParam(name = "maxPrice") OptionalDouble maxPrice) {
-
-        return service.findAll(minPrice, maxPrice);
+    List<Wine> findAll() {
+        return service.findAll();
     }
 
-    @GetMapping("wines/{name}")
+    @GetMapping("wines/name/{name}")
     List<Wine> findByName(@PathVariable String name) {
         return service.findByName(name);
     }
 
-    @GetMapping("wines/{type}")
+    @GetMapping("wines/type/{type}")
     List<Wine> findByType(@PathVariable WineType type) {
         return service.findByType(type);
     }
 
-    @GetMapping("wines/{country}")
+    @GetMapping("wines/country/{country}")
     List<Wine> findByCountry(@PathVariable String country) {
         return service.findByCountry(country);
     }
 
-    @GetMapping("wines/{quantity}")
+    @GetMapping("wines/quantity/{quantity}")
     List<Wine> findByQuantity(@PathVariable Double quantity) {
         return service.findByQuantity(quantity);
     }
 
-    @GetMapping("wines/{foodPairings}")
+    @GetMapping("/wines/price")
+    List<Wine> findByPrice(@RequestParam(name = "minPrice") OptionalDouble minPrice,
+                           @RequestParam(name = "maxPrice") OptionalDouble maxPrice) {
+
+        return service.findByPrice(minPrice, maxPrice);
+    }
+
+    @GetMapping("wines/pairings/{foodPairings}")
     List<Wine> findByFoodPairings(@RequestParam List<String> foodPairings) {
+        // Note: HTTP request has to be in the following format:
+        // ?id=1,2,3 and NOT like this ?id=1&id=2&id=3
         return service.findByFoodPairings(foodPairings);
     }
 
     @GetMapping("wines/{id}")
-    Wine findById(@PathVariable Long id) {
-        return service.findById(id)
+    EntityModel<Wine> findById(@PathVariable Long id) {
+        Wine wine = service.findById(id)
                 .orElseThrow(() -> new WineNotFoundException(id));
+
+        return new EntityModel<>(wine,
+                linkTo(methodOn(WineController.class).findById(id)).withSelfRel(),
+                linkTo(methodOn(WineController.class).findAll()).withRel("wines"));
     }
 
     @PostMapping("/wines")
