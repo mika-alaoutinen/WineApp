bf18a6abd00329f2e6bda03a664ea0c683f0f605
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2019-11-01 21:19:01 +0200

    Wine (#10)
    * Adding more functionality for searching wines.
    
    * Adding more search functionality to wines.
    
    * Hateoas (#9)

--- src/main/java/com/mika/WineApp/controllers/WineController.java
+++ src/main/java/com/mika/WineApp/controllers/WineController.java
@@ -1,44 +1,99 @@
 package com.mika.WineApp.controllers;
 
 import com.mika.WineApp.errors.WineNotFoundException;
+import com.mika.WineApp.hateoas.WineModelAssembler;
 import com.mika.WineApp.models.Wine;
+import com.mika.WineApp.models.WineType;
 import com.mika.WineApp.repositories.WineRepository;
 import com.mika.WineApp.services.WineService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.OptionalDouble;
 
 @RestController
 public class WineController {
     private final WineService service;
+    private final WineModelAssembler assembler;
 
-    public WineController(WineRepository repository) {
+    public WineController(WineRepository repository, WineModelAssembler assembler) {
         this.service = new WineService(repository);
+        this.assembler = assembler;
     }
 
     @GetMapping("/wines")
-    List<Wine> findAll() {
-        return service.findAll();
+    public CollectionModel<EntityModel<Wine>> findAll() {
+        var wines = service.findAll();
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("wines/name/{name}")
+    public CollectionModel<EntityModel<Wine>> findByName(@PathVariable String name) {
+        var wines = service.findByName(name);
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("wines/type/{type}")
+    public CollectionModel<EntityModel<Wine>> findByType(@PathVariable WineType type) {
+        var wines = service.findByType(type);
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("wines/country/{country}")
+    public CollectionModel<EntityModel<Wine>> findByCountry(@PathVariable String country) {
+        var wines = service.findByCountry(country);
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("wines/quantity/{quantity}")
+    public CollectionModel<EntityModel<Wine>> findByQuantity(@PathVariable Double quantity) {
+        var wines = service.findByQuantity(quantity);
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("/wines/price")
+    public CollectionModel<EntityModel<Wine>> findByPrice(
+            @RequestParam(name = "minPrice") OptionalDouble minPrice,
+            @RequestParam(name = "maxPrice") OptionalDouble maxPrice) {
+
+        var wines = service.findByPrice(minPrice, maxPrice);
+        return assembler.buildResponse(wines);
+    }
+
+    @GetMapping("wines/pairings/{foodPairings}")
+    public CollectionModel<EntityModel<Wine>> findByFoodPairings(
+            @RequestParam List<String> foodPairings) {
+        // Note: HTTP request has to be in the following format:
+        // ?id=1,2,3 and NOT like this ?id=1&id=2&id=3
+
+        var wines = service.findByFoodPairings(foodPairings);
+        return assembler.buildResponse(wines);
     }
 
     @GetMapping("wines/{id}")
-    Wine find(@PathVariable Long id) {
-        return service.find(id)
-                      .orElseThrow(() -> new WineNotFoundException(id));
+    public EntityModel<Wine> findById(@PathVariable Long id) {
+        Wine wine = service.findById(id)
+                .orElseThrow(() -> new WineNotFoundException(id));
+
+        return assembler.toModel(wine);
     }
 
     @PostMapping("/wines")
-    Wine add(@RequestBody Wine newWine) {
-        return service.add(newWine);
+    public EntityModel<Wine> add(@RequestBody Wine newWine) {
+        Wine wine = service.add(newWine);
+        return assembler.toModel(wine);
     }
 
     @PutMapping("wines/{id}")
-    Wine edit(@RequestBody Wine editedWine, @PathVariable Long id) {
-        return service.edit(editedWine, id);
+    public EntityModel<Wine> edit(@RequestBody Wine editedWine, @PathVariable Long id) {
+        Wine wine = service.edit(editedWine, id);
+        return assembler.toModel(wine);
     }
 
     @DeleteMapping("wines/{id}")
-    void delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id) {
         service.delete(id);
     }
 }
--- src/main/java/com/mika/WineApp/services/WineService.java
+++ src/main/java/com/mika/WineApp/services/WineService.java
@@ -1,10 +1,12 @@
 package com.mika.WineApp.services;
 
 import com.mika.WineApp.models.Wine;
+import com.mika.WineApp.models.WineType;
 import com.mika.WineApp.repositories.WineRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 public class WineService {
     private final WineRepository repository;
@@ -13,14 +15,42 @@ public class WineService {
         this.repository = repository;
     }
 
+// Find wines:
     public List<Wine> findAll() {
         return repository.findAll();
     }
 
-    public Optional<Wine> find(Long id) {
+    public List<Wine> findByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public List<Wine> findByType(WineType type) {
+        return repository.findByType(type);
+    }
+
+    public List<Wine> findByCountry(String country) {
+        return repository.findByCountry(country);
+    }
+
+    public List<Wine> findByQuantity(double quantity) {
+        return repository.findByQuantity(quantity);
+    }
+
+    public List<Wine> findByPrice(OptionalDouble minPrice, OptionalDouble maxPrice) {
+        double min = minPrice.orElse(0);
+        double max = maxPrice.orElse(9999);
+        return repository.findByPriceBetween(min, max);
+    }
+
+    public List<Wine> findByFoodPairings(List<String> foodPairings) {
+        return repository.findByFoodPairingsIn(foodPairings);
+    }
+
+    public Optional<Wine> findById(Long id) {
         return repository.findById(id);
     }
 
+// Add, edit and delete:
     public Wine add(Wine newWine) {
         return repository.save(newWine);
     }
@@ -36,7 +66,6 @@ public class WineService {
                 wine.setFoodPairings(editedWine.getFoodPairings());
                 wine.setUrl(editedWine.getUrl());
         });
-
         return editedWine;
     }
 
