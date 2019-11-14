b0fecd2026bff6ba80a9d709a8147255d71a5225
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-14 19:09:23 +0200

    Added more expections handling.

--- src/main/java/com/mika/WineApp/errors/InvalidWineTypeAdvice.java
+++ src/main/java/com/mika/WineApp/errors/InvalidWineTypeAdvice.java
@@ -1,18 +1,15 @@
 package com.mika.WineApp.errors;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
-public class ReviewNotFoundAdvice {
-
+public class InvalidWineTypeAdvice {
     @ResponseBody
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String reviewNotFoundHandler(ReviewNotFoundException e) {
+    String invalidWineTypeHandler(InvalidWineTypeException e) {
         return e.getMessage();
     }
 }
