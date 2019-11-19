a8b84e440452c9e210ac245dfe41cdf6a7bf3b76
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-11-19 21:12:40 +0200

    Added CORS filter for localhost:8081.

--- src/main/java/com/mika/WineApp/configuration/WebSecurityConfig.java
+++ src/main/java/com/mika/WineApp/configuration/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.mika.WineApp.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 /**
  * The web security config is used to disable Spring CSRF.
@@ -15,5 +23,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+    }
+
+    /**
+     * Configure CORS to allow connections from Vue client that is running on port 8081.
+     * This configuration is done to fix a CORS error that prevents the VUE client from
+     * sending requests to the backend REST API.
+     * @return FilterRegistrationBean.
+     */
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        var config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+
+        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
