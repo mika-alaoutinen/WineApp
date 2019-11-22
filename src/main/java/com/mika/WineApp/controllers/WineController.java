d95737d716ae679fbd480d5f32b11f6754fda294
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2019-11-22 18:53:34 +0200

    Rm hateoas (#22)
    * Removed HATEOAS support from review controller.
    
    * Removed HATEOAS support from wine controller.
    
    * Deleted model assemblers.
    
    * Moved frontend server URL to application.properties.

--- src/main/java/com/mika/WineApp/controllers/WineController.java
+++ src/main/java/com/mika/WineApp/controllers/WineController.java
@@ -1,12 +1,9 @@
 package com.mika.WineApp.controllers;
 
 import com.mika.WineApp.errors.WineNotFoundException;
-import com.mika.WineApp.hateoas.WineModelAssembler;
 import com.mika.WineApp.models.Wine;
 import com.mika.WineApp.repositories.WineRepository;
 import com.mika.WineApp.services.WineService;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,90 +12,76 @@ import java.util.List;
 @RestController
 public class WineController {
     private final WineService service;
-    private final WineModelAssembler assembler;
     private static final String baseUrl = "/wines";
 
-    public WineController(WineRepository repository, WineModelAssembler assembler) {
+    public WineController(WineRepository repository) {
         this.service = new WineService(repository);
-        this.assembler = assembler;
     }
 
 // --- Find wines ---
     @GetMapping(baseUrl)
-    public CollectionModel<EntityModel<Wine>> findAll() {
-        var wines = service.findAll();
-        return assembler.buildResponse(wines);
+    public List<Wine> findAll() {
+        return service.findAll();
     }
 
     @GetMapping(baseUrl + "/name/{name}")
-    public CollectionModel<EntityModel<Wine>> findByName(@PathVariable String name) {
-        var wines = service.findByName(name);
-        return assembler.buildResponse(wines);
+    public List<Wine> findByName(@PathVariable String name) {
+        return service.findByName(name);
     }
 
     @GetMapping(baseUrl + "/type/{type}")
-    public CollectionModel<EntityModel<Wine>> findByType(@PathVariable String type) {
-        var wines = service.findByType(type);
-        return assembler.buildResponse(wines);
+    public List<Wine> findByType(@PathVariable String type) {
+        return service.findByType(type);
     }
 
     @GetMapping(baseUrl + "/country/{country}")
-    public CollectionModel<EntityModel<Wine>> findByCountry(@PathVariable String country) {
-        var wines = service.findByCountry(country);
-        return assembler.buildResponse(wines);
+    public List<Wine> findByCountry(@PathVariable String country) {
+        return service.findByCountry(country);
     }
 
     @GetMapping(baseUrl + "/quantity/{quantity}")
-    public CollectionModel<EntityModel<Wine>> findByQuantity(@PathVariable Double quantity) {
-        var wines = service.findByQuantity(quantity);
-        return assembler.buildResponse(wines);
+    public List<Wine> findByQuantity(@PathVariable Double quantity) {
+        return service.findByQuantity(quantity);
     }
 
     @GetMapping(baseUrl + "/price")
-    public CollectionModel<EntityModel<Wine>> findByPrice(
+    public List<Wine> findByPrice(
             @RequestParam(name = "minPrice", defaultValue = "0") double minPrice,
             @RequestParam(name = "maxPrice", defaultValue = "9999") double maxPrice) {
 
-        var wines = service.findByPrice(minPrice, maxPrice);
-        return assembler.buildResponse(wines);
+        return service.findByPrice(minPrice, maxPrice);
     }
 
     @GetMapping(baseUrl + "/description")
-    public CollectionModel<EntityModel<Wine>> findByDescription(
+    public List<Wine> findByDescription(
             @RequestParam(name = "desc") List<String> description) {
 
-        var wines = service.findByDescription(description);
-        return assembler.buildResponse(wines);
+        return service.findByDescription(description);
     }
 
     @GetMapping(baseUrl + "/pairing")
-    public CollectionModel<EntityModel<Wine>> findByFoodPairings(
+    public List<Wine> findByFoodPairings(
             @RequestParam(name = "pair") List<String> foodPairings) {
 
-        var wines = service.findByFoodPairings(foodPairings);
-        return assembler.buildResponse(wines);
+        return service.findByFoodPairings(foodPairings);
     }
 
     @GetMapping(baseUrl + "/{id}")
-    public EntityModel<Wine> findById(@PathVariable Long id) {
-        Wine wine = service.findById(id)
+    public Wine findById(@PathVariable Long id) {
+        return service.findById(id)
                 .orElseThrow(() -> new WineNotFoundException(id));
-
-        return assembler.toModel(wine);
     }
 
 // --- Add, edit and delete ---
     @PostMapping(baseUrl)
     @ResponseStatus(HttpStatus.CREATED)
-    public EntityModel<Wine> add(@RequestBody Wine newWine) {
-        Wine wine = service.add(newWine);
-        return assembler.toModel(wine);
+    public Wine add(@RequestBody Wine newWine) {
+        return service.add(newWine);
     }
 
     @PutMapping(baseUrl + "/{id}")
-    public EntityModel<Wine> edit(@RequestBody Wine editedWine, @PathVariable Long id) {
-        Wine wine = service.edit(editedWine, id);
-        return assembler.toModel(wine);
+    public Wine edit(@RequestBody Wine editedWine, @PathVariable Long id) {
+        return service.edit(editedWine, id);
     }
 
     @DeleteMapping(baseUrl + "/{id}")
