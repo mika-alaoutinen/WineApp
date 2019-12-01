9693588094a7ec78ce9c214df6a3ee1ee7daeaf2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-01 13:47:31 +0200

    Added EntityModel super class and CrudService interface.

---
+++ src/main/java/com/mika/WineApp/models/EntityModel.java
@@ -0,0 +1,18 @@
+package com.mika.WineApp.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * A super class for all persisted entities (at the moment Wine and Review).
+ * Contains the ID attribute.
+ */
+@Data
+@MappedSuperclass
+public abstract class EntityModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(updatable = false, nullable = false)
+    private Long id;
+}
