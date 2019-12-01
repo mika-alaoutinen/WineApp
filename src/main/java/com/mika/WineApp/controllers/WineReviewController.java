028ee43a57f931eba821af5bf3961b229a1f2167
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-01 13:50:47 +0200

    Refactored WineReviewController.

--- src/main/java/com/mika/WineApp/controllers/WineReviewController.java
+++ src/main/java/com/mika/WineApp/controllers/WineReviewController.java
@@ -3,7 +3,7 @@ package com.mika.WineApp.controllers;
 import com.mika.WineApp.dtos.WineReviewDto;
 import com.mika.WineApp.models.Wine;
 import com.mika.WineApp.repositories.WineRepository;
-import com.mika.WineApp.services.WineService;
+import com.mika.WineApp.services.WineServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,20 +12,20 @@ import java.util.stream.Collectors;
 
 @RestController
 public class WineReviewController {
-    private final WineService wineService;
+    private final WineServiceImpl wineServiceImpl;
 
     public WineReviewController(WineRepository wineRepository) {
-        this.wineService = new WineService(wineRepository);
+        this.wineServiceImpl = new WineServiceImpl(wineRepository);
     }
 
     @GetMapping("/winereviews")
     public List<WineReviewDto> findAll() {
-        return mapToDto(wineService.findAll());
+        return mapToDto(wineServiceImpl.findAll());
     }
 
     @GetMapping("/winereviews/{wineName}")
     public List<WineReviewDto> findByName(String wineName) {
-        return mapToDto(wineService.findByName(wineName));
+        return mapToDto(wineServiceImpl.findByName(wineName));
     }
 
     private List<WineReviewDto> mapToDto(List<Wine> wines) {
