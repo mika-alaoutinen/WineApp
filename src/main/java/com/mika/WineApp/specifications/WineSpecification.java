5415a22645e068d4a6b09c79ddc4d23cff9f95bc
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2019-12-21 20:59:38 +0200

    WIP 2 wine search.

--- src/main/java/com/mika/WineApp/specifications/WineSpecification.java
+++ src/main/java/com/mika/WineApp/specifications/WineSpecification.java
@@ -1,9 +1,11 @@
 package com.mika.WineApp.specifications;
 
 import com.mika.WineApp.models.Wine;
+import com.mika.WineApp.models.WineType;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WineSpecification implements Specification<Wine> {
@@ -18,45 +20,29 @@ public class WineSpecification implements Specification<Wine> {
         this.wine = wine;
     }
 
-    private void test(Root<Wine> root, CriteriaBuilder builder, String rootAttribute, String wineAttribute, Predicate predicate) {
-//        List<Predicate> predicates = List.of(builder.like(rootCountry, wineCountry), builder.like(rootName, wineName));
-//        predicate.getExpressions().addAll(predicates);
-
-        Expression<String> expression = builder.lower(root.get(rootAttribute));
-
-        predicate.getExpressions()
-                 .add(builder.like(expression, wineAttribute));
-    }
-
     public Predicate toPredicate(Root<Wine> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        Predicate predicate = builder.conjunction();
+        Expression<String> rootName = builder.lower(root.get("name"));
+        String wineName = formatString(wine.getName());
 
-        if (wine.getName() != null) {
-            Expression<String> rootName = builder.lower(root.get("name"));
-            String wineName = formatString(wine.getName());
+        Expression<String> rootCountry = builder.lower(root.get("country"));
+        String wineCountry = formatString(wine.getCountry());
 
-            predicate.getExpressions()
-                     .add(builder.like(rootName, wineName));
-        }
+        List<Predicate> predicates = new ArrayList<>(List.of(
+                builder.like(rootName, wineName),
+                builder.like(rootCountry, wineCountry)));
+
 
         if (wine.getType() != null) {
-            predicate.getExpressions()
-                     .add(builder.equal(root.get("type"), wine.getType()));
-        }
-
-        if (wine.getCountry() != null) {
-            Expression<String> rootCountry = builder.lower(root.get("country"));
-            String wineCountry = formatString(wine.getCountry());
-
-            predicate.getExpressions()
-                     .add(builder.like(rootCountry, wineCountry));
+            predicates.add(builder.equal(root.get("type"), wine.getType()));
         }
 
         // minPrice, maxPrice and volumes
         if (minPrice != null && maxPrice != null) {
-            predicate.getExpressions()
-                    .add(builder.between(root.get("price"), minPrice, maxPrice));
+            predicates.add(builder.between(root.get("price"), minPrice, maxPrice));
         }
+
+        Predicate predicate = builder.conjunction();
+        predicate.getExpressions().addAll(predicates);
 
         return predicate;
     }
@@ -68,5 +54,15 @@ public class WineSpecification implements Specification<Wine> {
      */
     private String formatString(String string) {
         return string == null ? "%" : "%" + string.toLowerCase() + "%";
+    }
+
+    private void test(Root<Wine> root, CriteriaBuilder builder, String rootAttribute, String wineAttribute, Predicate predicate) {
+//        List<Predicate> predicates = List.of(builder.like(rootCountry, wineCountry), builder.like(rootName, wineName));
+//        predicate.getExpressions().addAll(predicates);
+
+        Expression<String> expression = builder.lower(root.get(rootAttribute));
+
+        predicate.getExpressions()
+                .add(builder.like(expression, wineAttribute));
     }
 }
