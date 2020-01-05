1b4fc7e306bed4b9b8e75e774574297e428a949b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-05 17:09:02 +0200

    Rewrote wine specification to use methods properly.

--- src/main/java/com/mika/WineApp/specifications/WineSpecification.java
+++ src/main/java/com/mika/WineApp/specifications/WineSpecification.java
@@ -13,6 +13,7 @@ public class WineSpecification extends SuperSpecification implements Specificati
     private List<String> countries;
     private List<Double> volumes;
     private Wine wine;
+    private List<Predicate> predicates;
 
     public WineSpecification(Wine wine, List<String> countries, List<Double> volumes, Integer[] priceRange) {
         super();
@@ -20,50 +21,73 @@ public class WineSpecification extends SuperSpecification implements Specificati
         this.volumes = volumes;
         this.priceRange = priceRange;
         this.wine = wine;
+        this.predicates = new ArrayList<>();
     }
 
     public Predicate toPredicate(Root<Wine> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        List<Predicate> predicates = new ArrayList<>();
-
-        // Name:
-        if (wine.getName() != null) {
-            Expression<String> rootName = builder.lower(root.get("name"));
-            predicates.add(builder.like(rootName, super.formatString(wine.getName())));
-        }
-
-        // Type:
-        if (wine.getType() != null) {
-            predicates.add(builder.equal(root.get("type"), wine.getType()));
-        }
-
-        // Country:
-        if (countries != null && !countries.isEmpty()) {
-            Expression<String> rootCountry = builder.lower(root.get("country"));
-
-            var countryPredicates = countries.stream()
-                    .map(super::formatString)
-                    .map(country -> builder.like(rootCountry, country))
-                    .collect(Collectors.toList());
-
-            Predicate disjunction = super.createDisjunction(builder, countryPredicates);
-            predicates.add(disjunction);
-        }
-
-        // Price range:
-        if (priceRange != null && priceRange.length == 2) {
-            predicates.add(builder.between(root.get("price"), priceRange[0], priceRange[1]));
-        }
-
-        // Volume:
-        if (volumes != null && !volumes.isEmpty()) {
-            var volumePredicates = volumes.stream()
-                    .map(volume -> builder.equal(root.get("volume"), volume))
-                    .collect(Collectors.toList());
-
-            Predicate disjunction = super.createDisjunction(builder, volumePredicates);
-            predicates.add(disjunction);
-        }
+        namePredicate(root, builder);
+        typePredicate(root, builder);
+        priceRangePredicate(root, builder);
+        countryPredicate(root, builder);
+        volumePredicate(root, builder);
 
         return super.createConjunction(builder, predicates);
+    }
+
+    private void namePredicate(Root<Wine> root, CriteriaBuilder builder) {
+        if (wine.getName() == null) {
+            return;
+        }
+
+        Expression<String> rootName = builder.lower(root.get("name"));
+        Predicate predicate = builder.like(rootName, super.formatString(wine.getName()));
+        predicates.add(predicate);
+    }
+
+    private void typePredicate(Root<Wine> root, CriteriaBuilder builder) {
+        if (wine.getType() == null) {
+            return;
+        }
+
+        Predicate predicate = builder.equal(root.get("type"), wine.getType());
+        predicates.add(predicate);
+    }
+
+    private void priceRangePredicate(Root<Wine> root, CriteriaBuilder builder) {
+        if (priceRange == null || priceRange.length != 2) {
+            return;
+        }
+
+        Predicate predicate = builder.between(root.get("price"), priceRange[0], priceRange[1]);
+        predicates.add(predicate);
+    }
+
+    private void countryPredicate(Root<Wine> root, CriteriaBuilder builder) {
+        if (countries == null || countries.isEmpty()) {
+            return;
+        }
+
+        Expression<String> rootCountry = builder.lower(root.get("country"));
+
+        var countryPredicates = countries.stream()
+                .map(super::formatString)
+                .map(country -> builder.like(rootCountry, country))
+                .collect(Collectors.toList());
+
+        Predicate predicate = super.createDisjunction(builder, countryPredicates);
+        predicates.add(predicate);
+    }
+
+    private void volumePredicate(Root<Wine> root, CriteriaBuilder builder) {
+        if (volumes == null || volumes.isEmpty()) {
+            return;
+        }
+
+        var volumePredicates = volumes.stream()
+                .map(volume -> builder.equal(root.get("volume"), volume))
+                .collect(Collectors.toList());
+
+        Predicate predicate = super.createDisjunction(builder, volumePredicates);
+        predicates.add(predicate);
     }
 }
