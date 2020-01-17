53606590b6beaa622216ecacd96e34ef324beb3a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-01-17 23:11:51 +0200

    More test cases.

--- src/test/java/com/mika/WineApp/controllers/WineControllerTest.java
+++ src/test/java/com/mika/WineApp/controllers/WineControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
@@ -37,12 +38,14 @@ public class WineControllerTest {
     @MockBean
     private WineRepository repository;
 
+    private List<String> countries;
     private List<String> descriptions;
     private List<String> foodPairings;
     private Wine wine;
     private final String url = "/wines";
 
     public WineControllerTest() {
+        this.countries = List.of("Espanja", "Italia", "Ranska");
         this.descriptions = List.of("puolikuiva", "sitruunainen", "yrttinen");
         this.foodPairings = List.of("kana", "kala", "seurustelujuoma");
         this.wine = new Wine("Valkoviini 1", WineType.WHITE, "Espanja", 8.75, 0.75, descriptions, foodPairings, "invalid");
@@ -70,6 +73,19 @@ public class WineControllerTest {
     }
 
     @Test
+    public void getCountries() throws Exception {
+        Mockito.when(repository.findAllCountries()).thenReturn(countries);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.get(url + "/countries"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        var response = parseWordsFromResponse(result);
+        Assertions.assertEquals(countries, response);
+    }
+
+    @Test
     public void getDescriptions() throws Exception {
         Mockito.when(repository.findAllDescriptions()).thenReturn(descriptions);
 
@@ -78,19 +94,32 @@ public class WineControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
-        String response = result.getResponse().getContentAsString();
-        var words = parseWordsFromResponse(response);
+        var response = parseWordsFromResponse(result);
+        Assertions.assertEquals(descriptions, response);
+    }
 
-        Assertions.assertEquals(descriptions, words);
+    @Test
+    public void getFoodPairings() throws Exception {
+        Mockito.when(repository.findAllFoodPairings()).thenReturn(foodPairings);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.get(url + "/food-pairings"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        var response = parseWordsFromResponse(result);
+        Assertions.assertEquals(foodPairings, response);
     }
 
     /**
-     * Parses response string into a list of strings.
-     * The response string is scrubbed of brackets and quotation marks.
-     * @param response given by controller.
+     * Parses result from controller into a list of strings.
+     * The result is turned into a string, which is then scrubbed of brackets and quotation marks.
+     * @param result given by controller.
      * @return List of words.
      */
-    private List<String> parseWordsFromResponse(String response) {
+    private List<String> parseWordsFromResponse(MvcResult result) throws UnsupportedEncodingException {
+        String response = result.getResponse().getContentAsString();
+
         String[] words = response
                 .substring(1, response.length() - 1)
                 .replace("\"", "")
