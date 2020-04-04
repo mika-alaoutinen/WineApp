52fa6186b4effa919219183cdf33c93414a94103
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-04 20:51:43 +0300

    Figured out correct way to use exception handlers.

--- src/main/java/com/mika/WineApp/errors/InvalidDateException.java
+++ src/main/java/com/mika/WineApp/errors/InvalidDateException.java
@@ -1,8 +1,12 @@
 package com.mika.WineApp.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidDateException extends RuntimeException {
 
     public InvalidDateException(String date) {
-        super("Error: could not parse date " + date + ". Enter date as yyyy-mm-dd.");
+        super("Error: could not parse date " + date + ".");
     }
 }
