442c891bcdc105b79b4166d4781b77a7f188432d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-05 11:46:55 +0300

    Remove unnecessary annotations from exceptions.

--- src/main/java/com/mika/WineApp/errors/InvalidWineTypeException.java
+++ src/main/java/com/mika/WineApp/errors/InvalidWineTypeException.java
@@ -1,5 +1,9 @@
 package com.mika.WineApp.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidWineTypeException extends RuntimeException {
 
     public InvalidWineTypeException(String type) {
--- src/main/java/com/mika/WineApp/errors/ReviewNotFoundAdvice.java
+++ src/main/java/com/mika/WineApp/errors/ReviewNotFoundAdvice.java
@@ -1,17 +1,11 @@
 package com.mika.WineApp.errors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
 public class ReviewNotFoundAdvice {
-
     @ResponseBody
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     String reviewNotFoundHandler(ReviewNotFoundException e) {
         return e.getMessage();
     }
