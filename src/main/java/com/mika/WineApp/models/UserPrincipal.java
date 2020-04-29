49ee55a781aed816402fe09ccd93312205310988
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-29 20:28:37 +0300

    Add UserPrincipal and edit UserServiceImpl.

---
+++ src/main/java/com/mika/WineApp/models/UserPrincipal.java
@@ -0,0 +1,54 @@
+package com.mika.WineApp.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal build(User user) {
+        var authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserPrincipal(user.getId(), user.getUsername(), user.getPassword(), authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/mika/WineApp/services/UserServiceImpl.java
+++ src/main/java/com/mika/WineApp/services/UserServiceImpl.java
@@ -2,26 +2,24 @@ package com.mika.WineApp.services;
 
 import com.mika.WineApp.errors.user.UserNotFoundException;
 import com.mika.WineApp.models.User;
+import com.mika.WineApp.models.UserPrincipal;
 import com.mika.WineApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> account = repository.findByUsername(username);
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(username));
 
-        return account.map(user -> org.springframework.security.core.userdetails.User
-                .builder()
-                .username(user.getUsername())
-                .build()
-        ).orElseThrow(() -> new UserNotFoundException(username));
+        return UserPrincipal.build(user);
     }
 
     public User addUser(User newAccount) {
