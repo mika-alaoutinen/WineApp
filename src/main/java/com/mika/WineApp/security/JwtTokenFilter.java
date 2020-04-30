16145ea012ab851d9745459ece2aff9ba6a154f1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-30 18:28:42 +0300

    Add JwtTokenFilter and JwtAuthEntryPoint.

--- src/main/java/com/mika/WineApp/security/JwtTokenFilter.java
+++ src/main/java/com/mika/WineApp/security/JwtTokenFilter.java
@@ -1,5 +1,11 @@
 package com.mika.WineApp.security;
 
+import com.mika.WineApp.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,19 +14,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
-
-//    private
+    private final JwtProvider tokenProvider;
+    private final UserService service;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        try {
-            String jwt = getJwt(request);
-        } catch (Exception e) {
+        String jwt = getJwt(request);
+        if (jwt != null && tokenProvider.validateJwtToken(jwt)) {
+            String username = tokenProvider.getUserNameFromToken(jwt);
+            UserDetails userDetails = service.loadUserByUsername(username);
 
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(getAuthToken(userDetails, request));
         }
+
+        filterChain.doFilter(request, response);
     }
 
     private String getJwt(HttpServletRequest request) {
@@ -29,5 +42,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         return authHeader != null && authHeader.startsWith("Bearer ")
                 ? authHeader.replace("Bearer ", "")
                 : null;
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthToken(UserDetails userDetails, HttpServletRequest request) {
+        var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return authToken;
     }
 }
