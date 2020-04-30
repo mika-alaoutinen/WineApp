0ee811c6ea359810cde4bce41c8390d8a29c5bb4
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-04-30 18:41:45 +0300

    Delete UserNotFoundException.

--- src/main/java/com/mika/WineApp/errors/notfound/NotFoundException.java
+++ src/main/java/com/mika/WineApp/errors/notfound/NotFoundException.java
@@ -1,6 +1,7 @@
 package com.mika.WineApp.errors.notfound;
 
 import com.mika.WineApp.models.review.Review;
+import com.mika.WineApp.models.user.User;
 import com.mika.WineApp.models.wine.Wine;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,5 +15,9 @@ public class NotFoundException extends RuntimeException {
 
     public NotFoundException(Review review, Long id) {
         super("Error: could not find review with id " + id);
+    }
+
+    public NotFoundException(User user, String username) {
+        super("Error: could not find user with username " + username);
     }
 }
--- src/main/java/com/mika/WineApp/services/impl/UserServiceImpl.java
+++ src/main/java/com/mika/WineApp/services/impl/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.mika.WineApp.services.impl;
 
-import com.mika.WineApp.errors.user.UserNotFoundException;
+import com.mika.WineApp.errors.notfound.NotFoundException;
 import com.mika.WineApp.models.user.User;
 import com.mika.WineApp.security.model.UserPrincipal;
 import com.mika.WineApp.repositories.UserRepository;
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = repository.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException(username));
+                .orElseThrow(() -> new NotFoundException(new User(), username));
 
         return UserPrincipal.build(user);
     }
