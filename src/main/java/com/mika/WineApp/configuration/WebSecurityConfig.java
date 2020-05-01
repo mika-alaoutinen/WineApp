d66b2c7a05163fa482ce3069e8fa3ece386166c9
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-01 15:36:30 +0300

    Add WebSecurityConfig interface for Javadoc.

--- src/main/java/com/mika/WineApp/configuration/WebSecurityConfig.java
+++ src/main/java/com/mika/WineApp/configuration/WebSecurityConfig.java
@@ -1,118 +1,63 @@
 package com.mika.WineApp.configuration;
 
-import com.mika.WineApp.security.JwtAuthEntryPoint;
-import com.mika.WineApp.security.JwtProvider;
 import com.mika.WineApp.security.JwtTokenFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.List;
+/**
+ * This interface is used to move Javadocs out of the implementing class.
+ */
+public interface WebSecurityConfig {
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@RequiredArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+// Configurations:
 
-    @Value("#{'${frontend.urls}'.split(',')}")
-    private List<String> allowedUrls;
+    /**
+     * Overrides the default implementation in WebSecurityConfigurerAdapter.
+     * Used to disable CSRF. Additionally, if Spring Security is enabled in application.properties,
+     * configures authorization rules.
+     * @param http HttpSecurity
+     * @throws Exception e
+     */
+    void configure(HttpSecurity http) throws Exception;
 
-    @Value("${spring.security.enabled}")
-    private boolean securityEnabled;
+    /**
+     * Configures the Spring AuthenticationManager with custom UserDetailsService implementation
+     * and sets the password encoder to BCrypt.
+     * @param authBuilder AuthenticationManagerBuilder
+     * @throws Exception e
+     */
+    void configure(AuthenticationManagerBuilder authBuilder) throws Exception;
 
-    private final JwtAuthEntryPoint unauthorizedHandler;
-    private final JwtProvider jwtProvider;
-    private final UserDetailsService service;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+// Beans:
 
-        if (securityEnabled) {
-            configureSecurity(http);
-        }
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder authBuilder) throws Exception {
-        authBuilder
-                .userDetailsService(service)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+    /**
+     * Creates the AuthenticationManagerBean used for user authentication.
+     * @return authenticationManagerBean
+     * @throws Exception e
+     */
+    AuthenticationManager authenticationManagerBean() throws Exception;
 
     /**
      * Configure CORS to allow connections from the frontend client. If this configuration
      * is missing, communication between client and server is blocked and a CORS error is shown.
      * @return FilterRegistrationBean.
      */
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        var config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(allowedUrls);
-        config.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    FilterRegistrationBean<CorsFilter> corsFilter();
 
-        var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return bean;
-    }
+    /**
+     * JwtTokenFilter extends OncePerRequestFilter. It is used to set user authentication token.
+     * @return token filter.
+     */
+    JwtTokenFilter tokenFilter();
 
     /**
      * Use BCrypt as the password encoder.
-     * @return PasswordEncoder.
+     * @return PasswordEncoder
      */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JwtTokenFilter tokenFilter() {
-        return new JwtTokenFilter(jwtProvider, service);
-    }
-
-    // Private methods:
-    private void configureSecurity(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .exceptionHandling()
-                .authenticationEntryPoint(unauthorizedHandler)
-                .and()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+    PasswordEncoder passwordEncoder();
 }
