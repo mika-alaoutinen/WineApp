f09622a5156122116b5610eb4a2f913c3ac8fd49
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-02 15:11:46 +0300

    Add UserDetailsServiceTest.

---
+++ src/test/java/com/mika/WineApp/services/UserDetailsServiceTest.java
@@ -0,0 +1,57 @@
+package com.mika.WineApp.services;
+
+import com.mika.WineApp.TestUtilities.TestData;
+import com.mika.WineApp.errors.notfound.NotFoundException;
+import com.mika.WineApp.models.user.User;
+import com.mika.WineApp.repositories.UserRepository;
+import com.mika.WineApp.services.impl.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDetailsServiceTest {
+    private static final User user = TestData.createTestUsers().get(0);
+
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserDetailsServiceImpl service;
+
+    @Test
+    public void loadUserByUsername() {
+        String username = user.getUsername();
+
+        Mockito.when(repository.findByUsername(username))
+               .thenReturn(Optional.of(user));
+
+        UserDetails userDetails = service.loadUserByUsername(user.getUsername());
+
+        verify(repository, times(1)).findByUsername(username);
+        assertEquals(username, userDetails.getUsername());
+    }
+
+    @Test
+    public void throwExceptionOnNonexistentUsername() {
+        String username = "nonexistent user";
+
+        Mockito.when(repository.findByUsername(username))
+               .thenReturn(Optional.empty());
+
+        Exception e = assertThrows(NotFoundException.class, () -> service.loadUserByUsername(username));
+        verify(repository, times(1)).findByUsername(username);
+        assertEquals("Error: could not find user with username " + username, e.getMessage());
+    }
+}
