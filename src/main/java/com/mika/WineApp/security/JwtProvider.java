1e6d65234a82b387974bbce0cb23ed22f3d21339
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-02 14:44:37 +0300

    Add JwtProvider interface.

--- src/main/java/com/mika/WineApp/security/JwtProvider.java
+++ src/main/java/com/mika/WineApp/security/JwtProvider.java
@@ -1,62 +1,27 @@
 package com.mika.WineApp.security;
 
-import com.mika.WineApp.security.model.UserPrincipal;
-import io.jsonwebtoken.*;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 
-import java.util.Date;
+public interface JwtProvider {
 
-@Slf4j
-@Component
-public class JwtProvider {
+    /**
+     * Generates a new JWT token from Authentication.
+     * @param authentication Authentication
+     * @return JWT token as string
+     */
+    String generateJwtToken(Authentication authentication);
 
-    @Value("${jwt.expiration}")
-    private int jwtExpiration;
+    /**
+     * Parses JWT token for username.
+     * @param token JWT token
+     * @return username
+     */
+    String getUserNameFromToken(String token);
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-
-    public String generateJwtToken(Authentication authentication) {
-        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
-
-        return Jwts.builder()
-                .setSubject(principal.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(getExpirationDate())
-                .signWith(SignatureAlgorithm.HS256, jwtSecret)
-                .compact();
-    }
-
-    public String getUserNameFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody().getSubject();
-    }
-
-    public boolean validateJwtToken(String authToken) {
-        try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-            return true;
-        } catch (SignatureException e) {
-            log.error("Invalid JWT signature: " + e.getMessage());
-        } catch (MalformedJwtException e) {
-            log.error("Invalid JWT token: " + e.getMessage());
-        } catch (ExpiredJwtException e) {
-            log.error("Expired JWT token: " + e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.error("Unsupported JWT token: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.error("JWT claims string is empty: " + e.getMessage());
-        }
-
-        return false;
-    }
-
-    private Date getExpirationDate() {
-        return new Date(new Date().getTime() + jwtExpiration);
-    }
+    /**
+     * Validates JWT token.
+     * @param authToken as string
+     * @return boolean indicating if token is valid
+     */
+    boolean validateJwtToken(String authToken);
 }
---
+++ src/main/java/com/mika/WineApp/security/JwtProviderImpl.java
@@ -0,0 +1,62 @@
+package com.mika.WineApp.security;
+
+import com.mika.WineApp.security.model.UserPrincipal;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JwtProviderImpl implements JwtProvider {
+
+    @Value("${jwt.expiration}")
+    private int jwtExpiration;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(getExpirationDate())
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    private Date getExpirationDate() {
+        return new Date(new Date().getTime() + jwtExpiration);
+    }
+}
