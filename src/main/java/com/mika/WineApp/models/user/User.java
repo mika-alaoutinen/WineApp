2c95a1086de47aa2db1f4c65f444463176d039be
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-03 16:51:59 +0300

    Add User to EntityModel.

--- src/main/java/com/mika/WineApp/models/user/User.java
+++ src/main/java/com/mika/WineApp/models/user/User.java
@@ -1,7 +1,7 @@
 package com.mika.WineApp.models.user;
 
+import com.mika.WineApp.models.review.Review;
 import com.mika.WineApp.models.superclasses.IdentityModel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -9,13 +9,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 import java.util.Set;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 public class User extends IdentityModel {
 
@@ -31,4 +31,14 @@ public class User extends IdentityModel {
     @ElementCollection
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     private Set<Role> roles;
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.roles = Set.of();
+        this.reviews = List.of();
+    }
 }
