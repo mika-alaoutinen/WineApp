43827b50caa8af2805f6145b3f566fdc9fe69d76
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-04 20:23:38 +0300

    New methods for AuthenticationController

--- src/main/java/com/mika/WineApp/controllers/AuthenticationController.java
+++ src/main/java/com/mika/WineApp/controllers/AuthenticationController.java
@@ -8,11 +8,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -22,6 +20,7 @@ import java.util.Set;
 public class AuthenticationController {
     private final UserService service;
 
+// Public operations available to everyone:
     @Operation(summary = "Login to receive JWT token", description = "Login with valid username and password. On successful login, API returns a JWT token.")
     @PostMapping("login")
     public JwtToken login(@Valid @RequestBody User user) {
@@ -34,9 +33,19 @@ public class AuthenticationController {
         return service.registerUser(newUser);
     }
 
+// Restricted operations limited to users with admin role:
+    @GetMapping("search/id/{id}")
+    public User findByUsername(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping("search/username/{username}")
+    public User findByUsername(@PathVariable String username) {
+        return service.findByUserName(username);
+    }
+
     @Operation(summary = "Update user's roles", description = "Replace user's old roles with roles given in request body.")
-    @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("{id}/roles")
     public User updateRoles(@PathVariable Long id, @RequestBody Set<Role> roles) {
         return service.updateRoles(id, roles);
     }
