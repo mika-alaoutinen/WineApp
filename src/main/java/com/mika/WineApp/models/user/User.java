019c886299a9dbdeb3d28222a3d4e9b39a013f75
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-04 20:23:07 +0300

    New user has a default role of USER.

--- src/main/java/com/mika/WineApp/models/user/User.java
+++ src/main/java/com/mika/WineApp/models/user/User.java
@@ -1,6 +1,6 @@
 package com.mika.WineApp.models.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mika.WineApp.models.review.Review;
 import com.mika.WineApp.models.superclasses.IdentityModel;
 import lombok.Data;
@@ -26,13 +26,13 @@ public class User extends IdentityModel {
 
     @NotBlank
     @Size(min = 6)
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
     @ElementCollection
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
-    private Set<Role> roles = Set.of();
+    private Set<Role> roles = Set.of(Role.ROLE_USER);
 
     @OneToMany(mappedBy = "user")
     private List<Review> reviews = List.of();
@@ -40,7 +40,7 @@ public class User extends IdentityModel {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.roles = Set.of();
+        this.roles = Set.of(Role.ROLE_USER);
         this.reviews = List.of();
     }
 }
