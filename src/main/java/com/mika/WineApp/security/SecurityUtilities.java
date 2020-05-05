d88da1aa9f78ae7f13b11708a2f8781077efd3ad
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-05 19:01:14 +0300

    Refactor SecurityUtilities EntityModel validation.

--- src/main/java/com/mika/WineApp/security/SecurityUtilities.java
+++ src/main/java/com/mika/WineApp/security/SecurityUtilities.java
@@ -1,9 +1,22 @@
 package com.mika.WineApp.security;
 
+import com.mika.WineApp.errors.badrequest.BadRequestException;
 import com.mika.WineApp.models.superclasses.EntityModel;
 
 public interface SecurityUtilities {
+    /**
+     * Retrieves the username from the SecurityContext.
+     * @return username as string
+     */
     String getUsernameFromSecurityContext();
-    boolean isUpdateRequestValid(EntityModel model);
 
+    /**
+     * Validates an edit or delete action on wines and reviews before committing it.
+     * Checks that the user was from a user that is either
+     * 1) an admin or
+     * 2) the owner of the item being edited.
+     * @param model wine or review
+     * @throws BadRequestException e
+     */
+    void validateUpdateRequest(EntityModel model);
 }
--- src/main/java/com/mika/WineApp/security/SecurityUtilitiesImpl.java
+++ src/main/java/com/mika/WineApp/security/SecurityUtilitiesImpl.java
@@ -1,21 +1,45 @@
 package com.mika.WineApp.security;
 
+import com.mika.WineApp.errors.badrequest.BadRequestException;
 import com.mika.WineApp.models.superclasses.EntityModel;
+import com.mika.WineApp.models.user.Role;
 import com.mika.WineApp.security.model.UserPrincipal;
+import com.mika.WineApp.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityUtilitiesImpl implements SecurityUtilities {
+    private final UserService service;
 
     public String getUsernameFromSecurityContext() {
         return getUserPrincipal().getUsername();
     }
 
-    public boolean isUpdateRequestValid(EntityModel model) {
-        Long ownerId = model.getUser().getId();
+    public void validateUpdateRequest(EntityModel model) {
         Long userId = getUserPrincipal().getId();
-        return ownerId.equals(userId);
+        boolean isAdmin = isUserAdmin(userId);
+        boolean isOwner = isUserOwnerOfModel(model, userId);
+
+        if (!isAdmin && !isOwner) {
+            throw new BadRequestException(model);
+        }
+    }
+
+    private boolean isUserAdmin(Long userId) {
+        return service
+                .findById(userId)
+                .getRoles()
+                .contains(Role.ROLE_ADMIN);
+    }
+
+    private boolean isUserOwnerOfModel(EntityModel model, Long userId) {
+        return model
+                .getUser()
+                .getId()
+                .equals(userId);
     }
 
     private UserPrincipal getUserPrincipal() {
