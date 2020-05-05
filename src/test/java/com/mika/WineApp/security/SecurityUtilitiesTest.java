305b6b5844cf24379cc33c8129ebe6a7e1273ac3
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-05 19:41:13 +0300

    WIP SecurityUtilitiesTest

---
+++ src/test/java/com/mika/WineApp/security/SecurityUtilitiesTest.java
@@ -0,0 +1,55 @@
+package com.mika.WineApp.security;
+
+import com.mika.WineApp.TestUtilities.TestData;
+import com.mika.WineApp.models.wine.Wine;
+import com.mika.WineApp.security.model.UserPrincipal;
+import com.mika.WineApp.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SecurityUtilitiesTest {
+    private static final String username = "test_user";
+
+    @Mock
+    private UserService service;
+
+    @InjectMocks
+    private SecurityUtilitiesImpl securityUtils;
+
+    @BeforeEach
+    public void setupMocks() {
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        UserPrincipal userPrincipal = mock(UserPrincipal.class);
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(userPrincipal);
+        when(userPrincipal.getUsername()).thenReturn(username);
+    }
+
+    @Test
+    public void getUsernameFromSecurityContext() {
+        String name = securityUtils.getUsernameFromSecurityContext();
+        assertEquals(username, name);
+    }
+
+    @Test
+    public void validateUpdateRequest() {
+        Wine wine = TestData.initWines().get(0);
+        securityUtils.validateUpdateRequest(wine);
+
+    }
+}
