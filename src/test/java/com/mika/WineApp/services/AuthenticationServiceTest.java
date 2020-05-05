9c000b2b8ff1d9f4745671d311ebcc52cb5b6ac0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-05-05 17:21:51 +0300

    Refactor service tests.

---
+++ src/test/java/com/mika/WineApp/services/AuthenticationServiceTest.java
@@ -0,0 +1,80 @@
+package com.mika.WineApp.services;
+
+import com.mika.WineApp.TestUtilities.TestData;
+import com.mika.WineApp.models.user.User;
+import com.mika.WineApp.security.JwtProvider;
+import com.mika.WineApp.security.model.JwtToken;
+import com.mika.WineApp.services.impl.AuthenticationServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthenticationServiceTest {
+    private final static String encodedPassword = "encoded_password";
+    private final static User user = TestData.createTestUsers().get(0);
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private JwtProvider jwtProvider;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private AuthenticationServiceImpl service;
+
+    @Test
+    public void loginUserReturnsJwtToken() {
+        var authToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+        Mockito.when(authenticationManager.authenticate(authToken))
+                .thenReturn(authToken);
+
+        Mockito.when(jwtProvider.generateJwtToken(authToken))
+                .thenReturn("Bearer jwt-token");
+
+        JwtToken token = service.login(user);
+
+        verify(authenticationManager, times(1)).authenticate(any(Authentication.class));
+        verify(jwtProvider, times(1)).generateJwtToken(any(Authentication.class));
+
+        assertEquals(authToken, SecurityContextHolder.getContext().getAuthentication());
+        assertEquals("Bearer jwt-token", token.getToken());
+    }
+
+    @Test
+    public void registerUser() {
+        User newUser = new User(user.getUsername(), encodedPassword);
+
+        Mockito.when(userService.save(any(User.class)))
+               .thenReturn(newUser);
+
+        Mockito.when(passwordEncoder.encode(user.getPassword()))
+                .thenReturn(encodedPassword);
+
+        User registeredUser = service.register(user);
+
+        verify(passwordEncoder, times(1)).encode(user.getPassword());
+        verify(userService, times(1)).save(newUser);
+        assertEquals(encodedPassword, registeredUser.getPassword());
+    }
+}
--- src/test/java/com/mika/WineApp/services/UserServiceTest.java
+++ src/test/java/com/mika/WineApp/services/UserServiceTest.java
@@ -6,43 +6,26 @@ import com.mika.WineApp.errors.notfound.NotFoundException;
 import com.mika.WineApp.models.user.Role;
 import com.mika.WineApp.models.user.User;
 import com.mika.WineApp.repositories.UserRepository;
-import com.mika.WineApp.security.JwtProvider;
-import com.mika.WineApp.security.model.JwtToken;
 import com.mika.WineApp.services.impl.UserServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     private final static User user = TestData.createTestUsers().get(0);
     private final static Long userId = user.getId();
-    private final static String encodedPassword = "encoded_password";
-
-    @Mock
-    AuthenticationManager authenticationManager;
-
-    @Mock
-    JwtProvider jwtProvider;
-
-    @Mock
-    PasswordEncoder passwordEncoder;
 
     @Mock
     UserRepository repository;
@@ -73,59 +56,16 @@ public class UserServiceTest {
     }
 
     @Test
-    public void loginUserReturnsJwtToken() {
-        var authToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-
-        Mockito.when(authenticationManager.authenticate(authToken))
-               .thenReturn(authToken);
-
-        Mockito.when(jwtProvider.generateJwtToken(authToken))
-               .thenReturn("Bearer jwt-token");
-
-        JwtToken token = service.loginUser(user);
-
-        verify(authenticationManager, times(1)).authenticate(any(Authentication.class));
-        verify(jwtProvider, times(1)).generateJwtToken(any(Authentication.class));
-
-        assertEquals(authToken, SecurityContextHolder.getContext().getAuthentication());
-        assertEquals("Bearer jwt-token", token.getToken());
-    }
-
-    @Test
-    public void registerUser() {
-        User newUser = new User(user.getUsername(), encodedPassword);
-
-        Mockito.when(repository.existsByUsername(user.getUsername()))
-               .thenReturn(false);
-
-        Mockito.when(passwordEncoder.encode(user.getPassword()))
-               .thenReturn(encodedPassword);
-
-        Mockito.when(repository.save(newUser))
-                .thenReturn(newUser);
-
-        User registeredUser = service.registerUser(user);
-
-        verify(repository, times(1)).existsByUsername(user.getUsername());
-        verify(passwordEncoder, times(1)).encode(user.getPassword());
-        verify(repository, times(1)).save(newUser);
-
-        assertEquals(encodedPassword, registeredUser.getPassword());
-        assertTrue(registeredUser.getRoles().contains(Role.ROLE_USER));
-        assertEquals(1, registeredUser.getRoles().size());
-    }
-
-    @Test
     public void throwsExceptionIfUserNameIsTaken() {
         String username = user.getUsername();
 
         Mockito.when(repository.existsByUsername(username))
                .thenReturn(true);
 
-        Exception e = assertThrows(BadRequestException.class, () -> service.registerUser(user));
-        assertEquals("Error: username " + username + " already exists!", e.getMessage());
+        Exception e = assertThrows(BadRequestException.class, () -> service.save(user));
 
-        verify(repository, times(1)).existsByUsername(user.getUsername());
+        assertEquals("Error: username " + username + " already exists!", e.getMessage());
+        verify(repository, times(1)).existsByUsername(username);
         verify(repository, times(0)).save(any(User.class));
     }
 
