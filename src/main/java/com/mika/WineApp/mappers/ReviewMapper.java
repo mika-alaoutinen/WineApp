e914bb9f28667ff0cf8315bfb73e7469779e7b16
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-29 19:59:05 +0300

    Use WineMapper in ReviewMapper

--- src/main/java/com/mika/WineApp/mappers/ReviewMapper.java
+++ src/main/java/com/mika/WineApp/mappers/ReviewMapper.java
@@ -4,7 +4,7 @@ import com.mika.WineApp.models.review.Review;
 import com.mika.model.ReviewDTO;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = WineMapper.class)
 public interface ReviewMapper {
     Review toModel(ReviewDTO dto);
 
