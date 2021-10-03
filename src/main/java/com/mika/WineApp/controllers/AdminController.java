b6501c44eff4e34ddb8ce1dbd230074d7b8b3725
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-10-03 16:30:41 +0300

    Implement admin controller

--- src/main/java/com/mika/WineApp/controllers/AdminController.java
+++ src/main/java/com/mika/WineApp/controllers/AdminController.java
@@ -1,46 +1,57 @@
 package com.mika.WineApp.controllers;
 
-import com.mika.WineApp.models.user.Role;
-import com.mika.WineApp.models.user.User;
+import com.mika.WineApp.mappers.RoleMapper;
+import com.mika.WineApp.mappers.UserMapper;
 import com.mika.WineApp.services.AdminService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.mika.api.AdminApi;
+import com.mika.model.RoleDTO;
+import com.mika.model.UserDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "admin/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
-@Tag(name = "Admin API", description = "Contains operations for managing users. All operations are restricted to admins.")
-public class AdminController {
+public class AdminController implements AdminApi {
+    private final RoleMapper roleMapper;
+    private final UserMapper userMapper;
     private final AdminService service;
 
-    @Operation(summary = "Find all users", description = "Find all saved users.")
-    @GetMapping()
-    public List<User> findAll() {
-        return service.findAll();
+    @Override
+    public ResponseEntity<UserDTO> findUserById(Long id) {
+        var user = userMapper.toUserDTO(service.findById(id));
+        return ResponseEntity.ok(user);
     }
 
-    @Operation(summary = "Find user by ID", description = "Find a user by user ID.")
-    @GetMapping("id/{id}")
-    public User findById(@PathVariable Long id) {
-        return service.findById(id);
+    @Override
+    public ResponseEntity<UserDTO> findUserByUsername(String username) {
+        var user = userMapper.toUserDTO(service.findByUserName(username));
+        return ResponseEntity.ok(user);
     }
 
-    @Operation(summary = "Find user by username", description = "Find a user by username.")
-    @GetMapping("username/{username}")
-    public User findByUsername(@PathVariable String username) {
-        return service.findByUserName(username);
+    @Override
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        var users = service
+                .findAll()
+                .stream()
+                .map(userMapper::toUserDTO)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(users);
     }
 
-    @Operation(summary = "Update user's roles", description = "Replace user's old roles with roles given in request body.")
-    @PutMapping("{id}/roles")
-    public User updateRoles(@PathVariable Long id, @Valid @RequestBody Set<Role> roles) {
-        return service.updateRoles(id, roles);
+    @Override
+    public ResponseEntity<UserDTO> updateRoles(Long id, Set<RoleDTO> newRoles) {
+        var roles = newRoles
+                .stream()
+                .map(roleMapper::toModel)
+                .collect(Collectors.toSet());
+
+        var updatedUser = service.updateRoles(id, roles);
+        return ResponseEntity.ok(userMapper.toUserDTO(updatedUser));
     }
 }
