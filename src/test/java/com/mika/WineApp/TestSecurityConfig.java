ab23e4ec2586ffd4f60643196ecc510a8a5f739f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-01-29 14:44:16 +0200

    Remove Import annotation

--- src/test/java/com/mika/WineApp/TestSecurityConfig.java
+++ src/test/java/com/mika/WineApp/TestSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // Disable Spring Security for unit tests
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/**");
--- src/test/java/com/mika/WineApp/controllers/WineControllerTest.java
+++ src/test/java/com/mika/WineApp/controllers/WineControllerTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,8 +27,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(WineCrudController.class)
-@ContextConfiguration(classes = {TestSecurityConfig.class, WineMapperImpl.class })
+@ContextConfiguration(classes = {TestSecurityConfig.class, WineMapperImpl.class, WineCrudController.class})
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(WineCrudController.class)
 class WineControllerTest {
