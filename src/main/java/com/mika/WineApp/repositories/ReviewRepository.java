b224f452603425e93bc09a575483fc30bf0ee209
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-01-29 15:36:32 +0200

    Remove spring-boot-starter-data-rest library

--- src/main/java/com/mika/WineApp/repositories/ReviewRepository.java
+++ src/main/java/com/mika/WineApp/repositories/ReviewRepository.java
@@ -5,17 +5,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource(collectionResourceRel = "reviews", path = "reviews")
+@Repository
 public interface ReviewRepository extends
         PagingAndSortingRepository<Review, Long>,
         JpaSpecificationExecutor<Review> {
 
     // Find reviews:
+    @Override
     Optional<Review> findById(Long id);
 
     List<Review> findAllByOrderByDateDesc();
@@ -24,6 +25,7 @@ public interface ReviewRepository extends
 
     List<Review> findByWineNameContainingIgnoreCase(String wineName);
 
+    @Override
     Review save(Review review);
 
     // Quick searches:
--- src/main/java/com/mika/WineApp/repositories/UserRepository.java
+++ src/main/java/com/mika/WineApp/repositories/UserRepository.java
@@ -2,11 +2,11 @@ package com.mika.WineApp.repositories;
 
 import com.mika.WineApp.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@RepositoryRestResource(exported = false)
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     Optional<User> findByUsername(String username);
