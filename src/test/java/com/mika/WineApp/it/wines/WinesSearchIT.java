0c64b880a7e04f912acf4c5d08907dd6b1787a7f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-02-04 18:00:02 +0200

    More wine search tests

--- src/test/java/com/mika/WineApp/it/wines/WinesSearchIT.java
+++ src/test/java/com/mika/WineApp/it/wines/WinesSearchIT.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @IntegrationTestRead
 class WinesSearchIT {
-    private static final String ENDPOINT = "/wines";
+    private static final String ENDPOINT = "/wines/search";
 
     @Autowired
     private UserRepository userRepository;
@@ -35,11 +35,50 @@ class WinesSearchIT {
     @Test
     void searchWinesByName() throws Exception {
         mvc
-                .perform(get(ENDPOINT + "/search?name=Valkoviini"))
+                .perform(get(ENDPOINT + "?name=Valkoviini"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()").value(2))
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[0].name").value("Valkoviini 1"))
                 .andExpect(jsonPath("$[1].name").value("Valkoviini 2"));
+    }
+
+    @Test
+    void searchWinesByType() throws Exception {
+        mvc
+                .perform(get(ENDPOINT + "?type=RED"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].type").value("RED"))
+                .andExpect(jsonPath("$[1].type").value("RED"));
+    }
+
+    @Test
+    void searchWinesByCountry() throws Exception {
+        mvc
+                .perform(get(ENDPOINT + "?countries=Espanja"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].country").value("Espanja"))
+                .andExpect(jsonPath("$[1].country").value("Espanja"));
+    }
+
+    @Test
+    void searchWinesByVolume() throws Exception {
+        mvc
+                .perform(get(ENDPOINT + "?volumes=0.75,1.0"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].volume").value(0.75))
+                .andExpect(jsonPath("$[1].volume").value(0.75));
+    }
+
+    @Test
+    void searchWinesByPriceRange() throws Exception {
+        mvc
+                .perform(get(ENDPOINT + "?priceRange=28,30"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1))
+                .andExpect(jsonPath("$[0].price").value(29.95));
     }
 }
