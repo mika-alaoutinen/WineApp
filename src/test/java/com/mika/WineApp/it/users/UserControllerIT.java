665e5aacae6d98d522556b36314c1049bbbd1196
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-02-10 17:49:42 +0200

    Rewrite user controller IT tests

--- src/test/java/com/mika/WineApp/it/users/UserControllerIT.java
+++ src/test/java/com/mika/WineApp/it/users/UserControllerIT.java
@@ -1,37 +1,17 @@
 package com.mika.WineApp.it.users;
 
-import com.mika.WineApp.it.IntegrationTest;
-import com.mika.WineApp.security.SecurityUtilities;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@IntegrationTest
-@WithMockUser
-class UserControllerIT {
+@WithUserDetails("test_user")
+class UserControllerIT extends UserTest {
     private static final String ENDPOINT = "/users";
-
-    @MockBean
-    private SecurityUtilities securityUtilities;
-
-    @Autowired
-    private MockMvc mvc;
-
-    @BeforeEach
-    void setupSecurityUtils() {
-        when(securityUtilities.getUsernameFromSecurityContext()).thenReturn("test_user");
-    }
 
     @Test
     void getUsername() throws Exception {
@@ -39,8 +19,6 @@ class UserControllerIT {
                 .perform(get(ENDPOINT + "/username"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("test_user")));
-
-        verify(securityUtilities).getUsernameFromSecurityContext();
     }
 
     @Test
@@ -49,7 +27,5 @@ class UserControllerIT {
                 .perform(get(ENDPOINT + "/loggedIn"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(is("true")));
-
-        verify(securityUtilities).getUsernameFromSecurityContext();
     }
 }
