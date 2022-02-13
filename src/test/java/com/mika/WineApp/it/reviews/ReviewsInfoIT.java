a67c8979e5e19ee2a805dfeb1c94846354aa5ed1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-02-13 14:25:39 +0200

    Update ReviewsInfoIT

--- src/test/java/com/mika/WineApp/it/reviews/ReviewsInfoIT.java
+++ src/test/java/com/mika/WineApp/it/reviews/ReviewsInfoIT.java
@@ -1,13 +1,20 @@
 package com.mika.WineApp.it.reviews;
 
+import com.mika.WineApp.it.IntegrationTestRead;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class ReviewsInfoIT extends ReviewTest {
+@IntegrationTestRead
+class ReviewsInfoIT {
     private static final String ENDPOINT = "/reviews";
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     void getReviewCount() throws Exception {
