c27541ad354d52ef5d4ca4d8d5294a94c8962d71
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-02-13 13:20:42 +0200

    Add data and schema sql files

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,50 @@
+-- Users
+CREATE TABLE IF NOT EXISTS users
+(
+    id       bigint primary key,
+    password varchar not null,
+    username varchar not null
+);
+
+CREATE TABLE IF NOT EXISTS user_role
+(
+    user_id bigint primary key,
+    roles   varchar
+);
+
+-- Wines
+CREATE TABLE IF NOT EXISTS wines
+(
+    id      bigint primary key,
+    country varchar          not null,
+    name    varchar          not null,
+    price   double precision not null,
+    type    varchar(10)      not null,
+    url     varchar,
+    volume  double precision not null,
+    user_id bigint           not null
+);
+
+CREATE TABLE IF NOT EXISTS wine_descriptions
+(
+    id          bigint,
+    description varchar
+);
+
+CREATE TABLE IF NOT EXISTS wine_food_pairings
+(
+    id            bigint,
+    food_pairings varchar
+);
+
+-- Reviews
+CREATE TABLE IF NOT EXISTS reviews
+(
+    id          bigint primary key,
+    author      varchar          not null,
+    date        date             not null,
+    rating      double precision not null,
+    review_text text             not null,
+    user_id     bigint           not null,
+    wine_id     bigint           not null
+);
