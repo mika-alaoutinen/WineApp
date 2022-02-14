ce3abe70e194bcc555534e083bc22384ffc77358
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-02-14 19:01:48 +0200

    Update ForbiddenException

--- src/main/java/com/mika/WineApp/errors/ForbiddenException.java
+++ src/main/java/com/mika/WineApp/errors/ForbiddenException.java
@@ -1,18 +1,12 @@
 package com.mika.WineApp.errors;
 
-import com.mika.WineApp.entities.Review;
-import com.mika.WineApp.entities.Wine;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
 
-    public ForbiddenException(Review review) {
-        super("Tried to modify a review that you do not own!");
-    }
-
-    public ForbiddenException(Wine wine) {
-        super("Tried to modify a wine that you do not own!");
+    public ForbiddenException(String resourceType) {
+        super(String.format("Tried to modify a %s that you do not own!", resourceType));
     }
 }
