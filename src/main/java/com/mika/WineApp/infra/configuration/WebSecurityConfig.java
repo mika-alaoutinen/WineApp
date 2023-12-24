6a783bbeb4040e78d982047c7a34bccf24d668fd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-12-24 15:25:52 +0200

    Fix CORS bean

--- src/main/java/com/mika/WineApp/infra/configuration/WebSecurityConfig.java
+++ src/main/java/com/mika/WineApp/infra/configuration/WebSecurityConfig.java
@@ -4,10 +4,8 @@ import com.mika.WineApp.infra.security.JwtProvider;
 import com.mika.WineApp.infra.security.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -22,8 +20,8 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -75,7 +73,7 @@ class WebSecurityConfig {
      * @return FilterRegistrationBean.
      */
     @Bean
-    FilterRegistrationBean<CorsFilter> corsFilter() {
+    CorsConfigurationSource corsConfig() {
         var config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(allowedUrls);
@@ -84,11 +82,7 @@ class WebSecurityConfig {
 
         var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
-        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return bean;
+        return source;
     }
 
     /**
