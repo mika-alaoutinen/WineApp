3cee98eee8793be4f57bc3a6fa8c70457f5a03ff
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-12-24 17:27:46 +0200

    Disable Spring security in tests

--- src/test/java/com/mika/WineApp/WebSecurityConfig.java
+++ src/test/java/com/mika/WineApp/WebSecurityConfig.java
@@ -2,15 +2,14 @@ package com.mika.WineApp;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @TestConfiguration
 public class WebSecurityConfig {
 
     // Disable Spring Security for unit tests
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).build();
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().anyRequest();
     }
 }
