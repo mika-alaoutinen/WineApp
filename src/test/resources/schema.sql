a86188bde853fad78c308834bad6455dc0a54ab9
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-12-24 15:45:11 +0200

    WIP fix broken tests

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,32 +1,32 @@
 -- Hibernate sequence for generating IDs.
 -- Start at 9 because data.sql persists 8 entities
 -- and I want to set their IDs explicitly.
-CREATE SEQUENCE hibernate_sequence START 9 INCREMENT 1;
+CREATE SEQUENCE hibernate_sequence START WITH 9 INCREMENT BY 1;
 
 -- Users
 CREATE TABLE IF NOT EXISTS users
 (
-    id       serial primary key,
+    id       serial  primary key,
     password varchar not null,
     username varchar not null
 );
 
 CREATE TABLE IF NOT EXISTS user_role
 (
-    user_id bigint primary key,
+    user_id bigint      primary key,
     roles   varchar
 );
 
 -- Wines
 CREATE TABLE IF NOT EXISTS wine
 (
-    id      serial primary key,
+    id      serial           primary key,
     country varchar          not null,
     name    varchar          not null,
-    price   double precision not null,
+    price   double           not null,
     type    varchar(10)      not null,
     url     varchar,
-    volume  double precision not null,
+    volume  double           not null,
     user_id bigint           not null
 );
 
@@ -45,10 +45,10 @@ CREATE TABLE IF NOT EXISTS wine_food_pairings
 -- Reviews
 CREATE TABLE IF NOT EXISTS review
 (
-    id          serial primary key,
+    id          serial           primary key,
     author      varchar          not null,
     date        date             not null,
-    rating      double precision not null,
+    rating      double           not null,
     review_text text             not null,
     user_id     bigint           not null,
     wine_id     bigint           not null
