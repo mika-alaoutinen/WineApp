44e5ab5ced435df24eb484796b02301511881a2e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2023-12-24 14:05:21 +0200

    WIP fix tests

--- src/test/java/com/mika/WineApp/WebSecurityConfig.java
+++ src/test/java/com/mika/WineApp/WebSecurityConfig.java
@@ -1,17 +1,16 @@
 package com.mika.WineApp;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     // Disable Spring Security for unit tests
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/**");
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).build();
     }
 }
